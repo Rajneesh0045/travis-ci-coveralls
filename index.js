08337a3be2f82b03cd948485bc025bb1b4349cad
Kartik Gupta <29kartik@gmail.com>
2020-04-06 16:09:59 +0530

    Update sub condition

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const division = (a, b) => (b !== 0 ? a / b : NaN);
 
 const multiplication = (a, b) => a * b;
 
-const substraction = (a, b) => (a > b ? a - b : b - a);
+const substraction = (a, b) => (a >= b ? a - b : b - a);
 
 module.exports = {
 	addition,
