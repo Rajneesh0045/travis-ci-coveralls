bbc40edb7ef1aa8cfe045d145b1890c865258e4e
rajneesh0045 <rajneeshkumarsingh0045@gmail.com>
2020-05-07 01:31:59 +0530

    jsdoc added

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+/**
+ * Module to perform basic calculation
+ * @module app
+ */
+
+/**
+ * Function for addition
+ * @param {Number} a - first number for addition
+ * @param {Number} b - second number for addition
+ */
 const addition = (a, b) => a + b;
 
+/**
+ * Function for division
+ * @param {Number} a - first number for division
+ * @param {Number} b - second number for division
+ */
 const division = (a, b) => (b !== 0 ? a / b : NaN);
 
+/**
+ * Function for multiplication
+ * @param {Number} a - first number for multiplication
+ * @param {Number} b - second number for multiplication
+ */
 const multiplication = (a, b) => a * b;
 
+/**
+ * Function for substraction
+ * @param {Number} a - first number for substraction
+ * @param {Number} b - second number for substraction
+ */
 const substraction = (a, b) => (a >= b ? a - b : b - a);
 
 module.exports = {
